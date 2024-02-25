85aadea405f4231f68da5aaaf7a890e3b4468af2
ToskePG <danilo.tosic.pmf@gmail.com>
2024-02-25 22:38:59 +0100

    Configures database and dependencies before start of the project.

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Caclulator functions
+"""
+
+def add(x, y):
+    """Add x and y and return result"""
+    return x + y
+
+def substract(x, y):
+    """Susbstract x from y and return results"""
+    return y - x
