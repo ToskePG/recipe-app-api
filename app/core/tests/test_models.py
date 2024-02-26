ee4efcf31b0e97c9fefb796684855471c4af0e24
ToskePG <danilo.tosic.pmf@gmail.com>
2024-02-26 23:38:21 +0100

    Fixes admin.py for LINT testing

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
     """Test models."""
-    
+
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
         email = 'test@example.com'
@@ -16,6 +16,6 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-        
+
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password), password)
