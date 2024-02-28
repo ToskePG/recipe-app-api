8a92efc7a64b1a08a0da763f0811ce25d27cd531
ToskePG <danilo.tosic.pmf@gmail.com>
2024-02-28 22:41:43 +0100

    Fixes linting errors for jobs to pass.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,9 +7,10 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
     """Tests for Django admin"""
-    
+
     def setUp(self):
         """Create user and client."""
         self.client = Client()
@@ -23,12 +24,12 @@ class AdminSiteTests(TestCase):
             password='testpass123',
             name='Test User'
         )
-        
+
     def test_users_list(self):
         """Test that users are listed on page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
-        
+
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
@@ -36,5 +37,5 @@ class AdminSiteTests(TestCase):
         """Test the edit user page works."""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
-        
+
         self.assertEqual(res.status_code, 200)
