698579d58ac38a3930f148a0073790e5e10fca41
ToskePG <danilo.tosic.pmf@gmail.com>
2024-02-28 22:00:12 +0100

    Creates first test for superuser.

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,20 @@
+"""
+Test for Django Admin modifications
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class AdminSiteTests(TestCase):
+    """Tests for Django admin"""
+    
+    def setUp(self):
+        """Create user and client."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123',
+        )
+        self.client.force_login(self.admin_user)
