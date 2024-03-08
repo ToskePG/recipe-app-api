24b584184702c3e94828fcfa86ae7847c6e6a353
ToskePG <danilo.tosic.pmf@gmail.com>
2024-03-08 02:14:44 +0100

    Adds public tests for the user API. Creates serializer for user and craetes views for ser API.

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+"""
+Viewvs for the user API
+"""
+from rest_framework import generics
+from user.serializers import UserSerliazer
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerliazer
