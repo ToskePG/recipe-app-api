52bb3f4230c2498542b41c923987eeecaaa63eb9
ToskePG <danilo.tosic.pmf@gmail.com>
2024-03-08 01:49:30 +0100

    Adds tests for existing email and password length.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -36,3 +36,32 @@ class PublicUserApiTests(TestCase):
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
+        
+    def test_user_with_email_exists_error(self):
+        """Test error returned if user with email exists"""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testPass123',
+            'name': 'Test name',
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short_error(self):
+        """Test if the password is too short"""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'test',
+            'name': 'Test name',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
+        
+        
