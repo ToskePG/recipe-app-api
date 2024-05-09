6728a54724358920b207f312cde59544a8a8d5a1
ToskePG <danilo.tosic.pmf@gmail.com>
2024-05-09 02:25:51 +0200

    Adds test for recipe details. Create recipe detail serializer. Creates view for the recipe detail serializer. makes RecipeDetailSerializer the default one, makes function to determan the if the action is for the list of recipes.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -19,3 +19,10 @@ class RecipeSerializer(serializers.ModelSerializer):
             'link',
         ]
         read_only_fields = ['id']
+        
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializers for recipe details."""
+    
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
+                
