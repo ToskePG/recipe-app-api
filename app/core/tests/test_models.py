9666251eb20cc886e1acad15a5ee3de3ab57efe3
ToskePG <danilo.tosic.pmf@gmail.com>
2024-06-26 01:30:38 +0200

    Creates tests and model for ingredients api. Registers to admin.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 """
-Test for models.
+Tests for models.
 """
+from decimal import Decimal
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -9,7 +10,7 @@ from core import models
 
 
 def create_user(email='user@example.com', password='testpass123'):
-    """Create and return new user."""
+    """Create a return a new user."""
     return get_user_model().objects.create_user(email, password)
 
 
@@ -17,44 +18,39 @@ class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successful(self):
-        """Test creating a user with an email is successful"""
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
-            password=password
+            password=password,
         )
 
         self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password), password)
+        self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """Test email is normalized for new users.
-        Only checking for email, password can be the same for all users
-        """
+        """Test email is normalized for new users."""
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['test3@EXAMPLE.COM', 'test3@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
-            ['TEST5@EXAMPLE.COM', 'TEST5@example.com'],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, 'samplePassword123')  # noqa: E501
+            user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
-        """Test that creating a new user
-        without an email raises a ValueError
-        """
+        """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'samplePassword123')
+            get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
-        """Test creating a superuser"""
+        """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
             'test@example.com',
-            'samplePassword123',
+            'test123',
         )
 
         self.assertTrue(user.is_superuser)
@@ -63,22 +59,32 @@ class ModelTests(TestCase):
     def test_create_recipe(self):
         """Test creating a recipe is successful."""
         user = get_user_model().objects.create_user(
-            'user@example.com',
-            'testPass123',
+            'test@example.com',
+            'testpass123',
         )
         recipe = models.Recipe.objects.create(
             user=user,
             title='Sample recipe name',
             time_minutes=5,
-            price=10.0,
-            description='Sample recipe description',
+            price=Decimal('5.50'),
+            description='Sample receipe description.',
         )
 
         self.assertEqual(str(recipe), recipe.title)
 
     def test_create_tag(self):
-        """Test creating a tag is succesful."""
+        """Test creating a tag is successful."""
         user = create_user()
         tag = models.Tag.objects.create(user=user, name='Tag1')
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
